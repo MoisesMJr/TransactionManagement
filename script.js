dcd6fe0ba0500cc0defd3393d16167acb1556fdd
MoisesMJr <moisesmanonjr@hotmail.com>
2024-12-30 13:18:43 -0500

    testing conversion function

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {
-    alert('Welcome to Docs in a Row Services!');
-});
+// document.addEventListener('DOMContentLoaded', () => {
+//     alert('Welcome to Docs in a Row Services!');
+// });
+
+function wonToDollars(){
+
+    let wonAmount = document.getElementById('wonAmount').value;
+
+    if(wonAmount === '' || isNaN(wonAmount) || wonAmount <= 0){
+        document.getElementById('result').innerText = "Please enter a valid amount!";
+        return;
+    }
+
+    const conversionRate = 0.00068122546;
+
+    let dollarsAmount = wonAmount * conversionRate;
+
+    document.getElementById('result').innerText = `${wonAmount} Won = ${dollarsAmount.toFixed(2)} USD`;
+    
+}
